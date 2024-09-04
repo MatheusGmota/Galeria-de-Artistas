058a458f4f2b1ad0a020810a466da5beb87e191b
MatheusGmota <livematgreg@gmail.com>
2024-09-04 10:59:18 -0300

    Lista de dados

---
+++ dados.js
@@ -0,0 +1,28 @@
+let dados = [
+    {
+        titulo: "Billie Elish",
+        idade: 22,
+        albuns: [
+            "HIT ME HARD AND SOFT", "HAPPIER THAN EVER", "WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?"
+        ],
+        desc: "Billie Eilish, com sua voz inconfundível e letras introspectivas, conquistou o mundo da música com uma rapidez impressionante. Sua sonoridade única, que mescla elementos do pop, eletrônico e alternativo, a tornou um ícone para uma geração que busca autenticidade e emoção nas canções. Com apenas alguns anos de carreira, Billie já coleciona diversos prêmios e recordes, além de uma legião de fãs apaixonados por sua música e estilo. Gostaria de saber mais sobre algum aspecto específico da carreira de Billie Eilish? Por exemplo, posso te contar mais sobre seus álbuns, suas letras, ou sua influência na cultura pop.",
+        href: "https://pt.wikipedia.org/wiki/Billie_Eilish",
+        img: "./assets/billie.jpeg",
+    },                    
+    {
+        titulo: "Tyler, The Creator",
+        idade: 32,
+        albuns: ["IGOR", "CAL ME IF YOU GET LOST", "FLOWER BOY"],
+        desc: 'Tyler, The Creator é um rapper, cantor, compositor, produtor musical e diretor de arte americano. Conhecido por sua versatilidade musical e estilo visual distintivo, ele é um dos nomes mais influentes do hip-hop alternativo. Com letras que exploram temas como identidade, sexualidade e questões sociais, Tyler conquistou um público fiel e desafiou os limites do gênero. Seus álbuns, como "IGOR" e "CALL ME IF YOU GET LOST", são considerados obras-primas do hip-hop contemporâneo. Além de sua carreira musical, Tyler é fundador da marca de streetwear GOLF WANG.',
+        href: "https://pt.wikipedia.org/wiki/Tyler,_the_Creator",
+        img: "./assets/tyler.jpg",
+    },
+    {
+        titulo: "Bruno Mars",
+        idade: 37,
+        albuns: ['24K Magic', 'Unorthodox Jukebox', 'Doo-Wops & Hooligans'],
+        desc: 'Bruno Mars é um cantor, compositor e produtor musical americano, conhecido por sua voz poderosa e performances energéticas. Com influências que vão do R&B ao pop, Bruno conquistou o mundo com hits como "Uptown Funk" e "Locked Out of Heaven". Seus shows são verdadeiras celebrações da música, com muita dança e coreografias elaboradas. Além de sua carreira solo, Bruno já colaborou com diversos artistas, como Mark Ronson e Cardi B.',
+        href: "https://pt.wikipedia.org/wiki/Bruno_Mars",
+        img: "./assets/bruno.jpg",
+    }
+]
